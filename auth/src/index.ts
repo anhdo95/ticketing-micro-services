a999bd5dc0380075a62ef5d811f7c8d9eaec1726
Anh Do <dinhanhdo95@gmail.com>
2023-04-30 17:33:54 +0700

    Connect to MongoDb and create a user

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import 'express-async-errors'
 import { json } from "body-parser";
+import mongoose from 'mongoose'
 import { currentUserRouter } from './routes/current-user';
 import { signupRouter } from './routes/signup'
 import { signinRouter } from './routes/signin';
@@ -10,7 +11,6 @@ import { NotFoundError } from "./errors/not-found-error";
 
 const app = express();
 app.use(json());
-
 
 app.use(currentUserRouter);
 app.use(signupRouter);
@@ -23,6 +23,17 @@ app.all('*', async () => {
 
 app.use(errorHandler)
 
-app.listen(3000, () => {
-  console.log("Listening on port 3000!");
-});
+async function start() {
+  try {
+    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth')
+    console.log('Connected to MongoDb')
+  } catch (error) {
+    console.log(error)
+  }
+  
+  app.listen(3000, () => {
+    console.log("Listening on port 3000!");
+  });
+}
+
+start()
