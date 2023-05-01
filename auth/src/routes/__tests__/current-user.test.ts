5a5c817fd4dafafc23546b9f02f15895e0bb067a
Anh Do <dinhanhdo95@gmail.com>
2023-05-01 21:13:02 +0700

    Use signin helper

--- auth/src/routes/__tests__/current-user.test.ts
+++ auth/src/routes/__tests__/current-user.test.ts
@@ -2,15 +2,7 @@ import request from "supertest";
 import { app } from "../../app";
 
 it("responds with details about the current user", async () => {
-  const signupResponse = await request(app)
-    .post("/api/users/signup")
-    .send({
-      email: "test@test.com",
-      password: "password",
-    })
-    .expect(201);
-
-  const cookie = signupResponse.get("Set-Cookie");
+  const cookie = await signin();
 
   const response = await request(app)
     .get("/api/users/currentuser")
@@ -18,4 +10,10 @@ it("responds with details about the current user", async () => {
     .expect(200);
 
   expect(response.body.currentUser.email).toBe("test@test.com");
+});
+
+it("responds with null if not authenticated", async () => {
+  const response = await request(app).get("/api/users/currentuser").expect(200);
+
+  expect(response.body.currentUser).toBeNull();
 });
--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,5 +1,11 @@
 import { MongoMemoryServer } from "mongodb-memory-server";
 import mongoose from "mongoose";
+import request from 'supertest'
+import { app } from '../app'
+
+declare global {
+  function signin(): Promise<string[]>
+}
 
 let mongo: any;
 beforeAll(async () => {
@@ -25,3 +31,16 @@ afterAll(async () => {
   }
   await mongoose.connection.close();
 });
+
+global.signin = async () => {
+  const response = await request(app)
+    .post("/api/users/signup")
+    .send({
+      email: "test@test.com",
+      password: "password",
+    })
+    .expect(201);
+
+  const cookie = response.get("Set-Cookie");
+  return cookie;
+};
