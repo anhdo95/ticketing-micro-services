fddf417eb2d198979f9d45a6dda47df26c9e70a4
Anh Do <dinhanhdo95@gmail.com>
2023-05-02 22:39:12 +0700

    Initialize a tickets service

---
+++ tickets/src/app.ts
@@ -0,0 +1,27 @@
+import express from "express";
+import 'express-async-errors'
+import { json } from "body-parser";
+import cookieSession from 'cookie-session'
+import { errorHandler, NotFoundError } from "@anhdo-tickets/common";
+
+import { createTicketRouter } from './routes/new'
+
+const app = express();
+
+app.set('trust proxy', 1)
+
+app.use(json());
+app.use(cookieSession({
+  signed: false,
+  secure: process.env.NODE_ENV !== 'test'
+}))
+
+app.use(createTicketRouter);
+
+app.all('*', async () => {
+  throw new NotFoundError()
+})
+
+app.use(errorHandler)
+
+export { app }
