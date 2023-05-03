191acac014e4b1278bd09a6c41763618216bbe44
Anh Do <dinhanhdo95@gmail.com>
2023-05-03 20:55:09 +0700

    Log undefined errors

--- common/middlewares/error-handler.ts
+++ common/middlewares/error-handler.ts
@@ -10,7 +10,7 @@ export function errorHandler(
     if (err instanceof CustomError) {
         return res.status(err.statusCode).send({ errors: err.serializeErrors() })
     }
-
+    console.error(err)
     res.status(400).send({
         errors: [{ message: err.message }]
     })
